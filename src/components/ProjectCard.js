191279089e3047ed1423790efff0bccf1c93a043
Mateus <mateus_b09@hotmail.com>
2024-04-13 17:44:18 -0300

    github links

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,18 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
-  return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
-    </Col>
-  )
-}
+export const ProjectCard = ({ title, description, imgUrl, githubUrl }) => {
+    return (
+        <Col size={12} sm={6} md={4}>
+            {/* Wrap the card content with an <a> tag */}
+            <a href={githubUrl} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+                <div className="proj-imgbx">
+                    <img src={imgUrl} alt={title} />
+                    <div className="proj-txtx">
+                        <h4>{title}</h4>
+                        <span>{description}</span>
+                    </div>
+                </div>
+            </a>
+        </Col>
+    );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,31 +19,37 @@ export const Projects = () => {
       title: "Book Analyzer",
       description: "Transform .txt book files into social media-style graphs, similar to Instagram",
       imgUrl: witcher,
+      githubUrl: "https://github.com/mateusb12/WitcherAnalysis",
     },
     {
       title: "Valorant Impact",
       description: "A tool that quantifies how much player actions can shift the odds of winning a Valorant round. By analyzing kills, weapon choices, economy and strategic moves, it reveals the real-time impact on victory chances.",
       imgUrl: data_analysis,
+      githubUrl: "https://github.com/mateusb12/valorant_impact",
     },
     {
       title: "Flight Scrapper",
       description: "Powered by Kiwi Tequilla API, this project scrapes flight data from the API and displays it in a user-friendly way. The main focus is to create alerts for the user when the price of a flight drops.",
       imgUrl: flight,
+      githubUrl: "https://github.com/mateusb12/TravelScrapper",
     },
     {
       title: "MCQ Quiz App",
       description: "A simple quiz app that I used for studying purposes. You can easily navigate through the questions and check your answers at the end.",
       imgUrl: quiz,
+      githubUrl: "https://github.com/mateusb12/JavascriptQuizApp",
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg2,
+      githubUrl: "https://github.com/mateusb12/",
     },
     {
       title: "Business Startup",
       description: "Design & Development",
       imgUrl: projImg3,
+      githubUrl: "https://github.com/mateusb12/",
     },
   ];
 
