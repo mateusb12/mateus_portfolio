b62d961855c78cc48abca4472adeccdc5e93a055
Mateus <mateus_b09@hotmail.com>
2024-04-19 15:14:56 -0300

    Refactor Skills component for language support and update language data - Introduced useContext, useState, and useEffect hooks for dynamic language selection in Skills.js - Fixed a typo from "Homer" to "Home" in the navbar.json file - Expanded skills.json to include a "skillMap" for language translation of skill titles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -22,104 +22,146 @@ import aws from "../assets/img//skills_icons/aws.png";
 import google from "../assets/img//skills_icons/google-cloud-small.png";
 import sql from "../assets/img//skills_icons/sql.png";
 
+import LanguageContext from './LanguageContext';
+import {useContext, useEffect, useState} from "react";
+import languageFile from "../data/skills.json";
+
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
+    const languageFile = require('../data/skills.json');
+    const { selectedFlag } = useContext(LanguageContext);
+    const [selectedLanguage, setSelectedLanguage] = useState('english');
+
+    useEffect(() => {
+        const languageKey = selectedFlag === 'usa' ? 'english' : 'portuguese';
+        setSelectedLanguage(languageKey);
+    }, [selectedFlag]);
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: {max: 4000, min: 3000},
+            items: 5
+        },
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: 3
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: 2
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: 1
+        }
+    };
+
+    const skillIcons = {
+        "Backend Development": server,
+        "APIs": api,
+        "Databases": database,
+        "Cloud": cloud,
+        "DevOps and CI/CD Pipelines": deploy,
+        "Security": lock,
+        "Python": python,
+        "Javascript": javascript,
+        "C#": csharp,
+        "Java": java,
+        "SQL": sql,
+        "HTML": html,
+        "CSS": css,
+        "GIT": git,
+        "Flask": flask,
+        "React": _react,
+        "JWT": jwt,
+        "Postgres": postgres,
+        "Docker": docker,
+        "AWS": aws,
+        "Google Cloud": google,
+    };
+
+    const skillsDataList = languageFile[selectedLanguage].key.skillList;
+    console.log(skillsDataList)
+    const skillsData = Object.entries(skillIcons).map(([title, src]) => ({
+        title,
+        src
+    }));
+
+    const skillMap = languageFile.skillMap;
+    const translateSkill = (skill) => {
+        return skillMap[skill] ? skillMap[skill][selectedLanguage] : skill;
     }
-  };
 
-    const skillsData = [
-        { src: server, title: "Backend Development" },
-        { src: api, title: "APIs" },
-        { src: database, title: "Databases" },
-        { src: cloud, title: "Cloud"},
-        { src: deploy, title: "DevOps and CI/CD Pipelines" },
-        { src: lock, title: "Security" },
-    ];
+    const basicTechnologiesList = languageFile[selectedLanguage].foundations.skillList;
+    const basicTechnologies = basicTechnologiesList.map((tech) => ({
+        title: tech,
+        src: skillIcons[tech]
+    }));
 
-    const basicTechnologies = [
-        { src: python, title: "Python" },
-        { src: javascript, title: "Javascript" },
-        { src: csharp, title: "C#" },
-        { src: java, title: "Java" },
-        { src: sql, title: "SQL" },
-        { src: html, title: "HTML" },
-        { src: css, title: "CSS" },
-    ];
+    const frameworksList = languageFile[selectedLanguage].stack.skillList;
+    const frameworks = frameworksList.map((framework) => ({
+        title: framework,
+        src: skillIcons[framework]
+    }));
 
-    const frameworks = [
-        { src: git, title: "GIT" },
-        { src: flask, title: "Flask" },
-        { src: _react, title: "React" },
-        { src: jwt, title: "JWT" },
-        { src: postgres, title: "Postgres" },
-        { src: docker, title: "Docker" },
-        { src: aws, title: "AWS" },
-        { src: google, title: "Google Cloud" },
-    ];
+    const keySkillsTitle = languageFile[selectedLanguage].key.title;
 
-  return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Key Skills</h2>
-                        <p>Core competencies in software development</p>
-                        <Carousel responsive={responsive} infinite={false}
-                                  className="owl-carousel owl-theme skill-slider">
-                            {skillsData.map((skill, index) => (
-                                <div className="item" key={index}>
-                                    <img src={skill.src} alt={skill.title}/>
-                                    <h5>{skill.title}</h5>
-                                </div>
-                            ))}
-                        </Carousel>
-                    </div>
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Programming Foundations</h2>
-                        <p>Essential programming languages that serves as foundations for using the frameworks</p>
-                        <Carousel responsive={responsive} infinite={false}
-                                  className="owl-carousel owl-theme skill-slider">
-                            {basicTechnologies.map((skill, index) => (
-                                <div className="item" key={index}>
-                                    <img src={skill.src} alt={skill.title}/>
-                                    <h5>{skill.title}</h5>
-                                </div>
-                            ))}
-                        </Carousel>
-                    </div>
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Technology Stack</h2>
-                        <p>Main tools for modern development</p>
-                        <Carousel responsive={responsive} infinite={false}
-                                  className="owl-carousel owl-theme skill-slider">
-                            {frameworks.map((skill, index) => (
-                                <div className="item" key={index}>
-                                    <img src={skill.src} alt={skill.title}/>
-                                    <h5>{skill.title}</h5>
-                                </div>
-                            ))}
-                        </Carousel>
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>{languageFile[selectedLanguage].key.title}</h2>
+                            <p>{languageFile[selectedLanguage].key.description}</p>
+                            <Carousel responsive={responsive} infinite={false}
+                                      className="owl-carousel owl-theme skill-slider">
+                                {skillsData.map((skill, index) => {
+                                    const translatedTitle = translateSkill(skill.title);
+                                    return (
+                                        <div className="item" key={index}>
+                                            <img src={skill.src} alt={translatedTitle}/>
+                                            <h5>{translatedTitle}</h5>
+                                        </div>
+                                    )
+                                })}
+                            </Carousel>
+                        </div>
+                        <div className="skill-bx wow zoomIn">
+                            <h2>{languageFile[selectedLanguage].foundations.title}</h2>
+                            <p>{languageFile[selectedLanguage].foundations.description}</p>
+                            <Carousel responsive={responsive} infinite={false}
+                                      className="owl-carousel owl-theme skill-slider">
+                                {basicTechnologies.map((skill, index) => {
+                                    const translatedTitle = translateSkill(skill.title);
+                                    return (
+                                        <div className="item" key={index}>
+                                            <img src={skill.src} alt={translatedTitle}/>
+                                            <h5>{translatedTitle}</h5>
+                                        </div>
+                                    )
+                                })}
+                            </Carousel>
+                        </div>
+                        <div className="skill-bx wow zoomIn">
+                            <h2>{languageFile[selectedLanguage].stack.title}</h2>
+                            <p>{languageFile[selectedLanguage].stack.description}</p>
+                            <Carousel responsive={responsive} infinite={false}
+                                      className="owl-carousel owl-theme skill-slider">
+                                {frameworks.map((skill, index) => {
+                                    const translatedTitle = translateSkill(skill.title);
+                                    return (
+                                        <div className="item" key={index}>
+                                            <img src={skill.src} alt={translatedTitle}/>
+                                            <h5>{translatedTitle}</h5>
+                                        </div>
+                                    )
+                                })}
+                            </Carousel>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
