7e2d2acf1493aeaea9f243b0b13bc1397ea80ae0
Mateus <matbessa12@gmail.com>
2024-08-28 17:52:50 -0300

    Update Introduction component and add styles for vertical index - Added a new `Introduction.css` file with styles for a vertical index and main container elements - Modified the `Introduction.js` component to utilize the new vertical index and main container structure - Removed unused state management and animation code, focusing the component on visibility tracking and content rendering

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,10 +2,12 @@ import { useState, useEffect, useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
+import '../css/Introduction.css';
 import TrackVisibility from 'react-on-screen';
 import LanguageContext from './LanguageContext';
 
 export const Introduction = () => {
+  const { selectedFlag } = useContext(LanguageContext);
   const languageContent = {
     english: {
       header: "Welcome to my Portfolio!",
@@ -21,69 +23,41 @@ export const Introduction = () => {
     }
   };
 
-  const { selectedFlag } = useContext(LanguageContext);
   const [selectedLanguage, setSelectedLanguage] = useState('english');
   const [introductionTexts, setIntroductionTexts] = useState(languageContent[selectedLanguage]);
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-  const period = 2000;
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const languageKey = selectedFlag === 'usa' ? 'english' : 'portuguese';
     setIntroductionTexts(languageContent[languageKey]);
+    setSelectedLanguage(languageKey);
   }, [selectedFlag]);
 
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker); };
-  }, [text]);
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  };
-
   return (
-      <section className="banner" id="home">
+      <section className="banner debug" id="home">
         <Container>
           <Row className="align-items-center">
             <Col xs={12} md={6} xl={7} className="mx-auto">
-              <TrackVisibility>
-                {({ isVisible }) =>
-                    <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                      <span className="tagline">{introductionTexts.header}</span>
-                      <h1>{introductionTexts.title}</h1>
-                      <p>{introductionTexts.body}</p>
-                      <button onClick={() => console.log('connect')}>{introductionTexts.footer} <ArrowRightCircle size={25}/>
-                      </button>
-                    </div>}
+              <TrackVisibility partialVisibility>
+                {({isVisible: visible}) => {
+                  if (visible !== isVisible) {
+                    setIsVisible(visible);
+                  }
+                  return (
+                      <div className="main-container">
+                        <span className="vertical-index"></span>
+                        <div className={isVisible ? "animate__animated animate__fadeIn main-body" : ""}>
+                          {/*<span className="tagline">{introductionTexts.header}</span>*/}
+                          <h1>{introductionTexts.title}</h1>
+                          <p>{introductionTexts.body}</p>
+                          <button onClick={() => console.log('connect')}>{introductionTexts.footer} <ArrowRightCircle
+                              size={25}/>
+                          </button>
+                        </div>
+
+                      </div>
+                  );
+                }}
               </TrackVisibility>
             </Col>
           </Row>
