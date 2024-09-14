3dd75625a418a00ae00496a980e80c7fe9834c3b
Mateus <matbessa12@gmail.com>
2024-09-14 13:24:03 -0300

    aaaaaaaaaaaaa

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -6,12 +6,12 @@ import { BsWordpress } from "react-icons/bs";
 import { FiFigma } from "react-icons/fi";
 import { FaReact, FaSass, FaBootstrap, FaGit } from "react-icons/fa";
 import {
-  SiTypescript,
-  SiMui,
-  SiNextdotjs,
-  SiVite,
-  SiStyledcomponents,
-  SiStorybook,
+    SiTypescript,
+    SiMui,
+    SiNextdotjs,
+    SiVite,
+    SiStyledcomponents,
+    SiStorybook,
 } from "react-icons/si";
 import "react-multi-carousel/lib/styles.css";
 import Carousel from "react-multi-carousel";
@@ -19,60 +19,60 @@ import "../css/ProjectCarousel.css";
 import ProjectCard from "./ProjectCard";
 
 const ProjectCarousel = () => {
-  const projects = [
-    <ProjectCard projectId="witcher" />,
-    <ProjectCard projectId="flight-scraper" />,
-  ];
+    const projects = [
+        <ProjectCard projectId="witcher" />,
+        <ProjectCard projectId="flight-scraper" />,
+    ];
 
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 3,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 3,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
 
-  return (
-    <section className="skill" id="skills">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="skill-bx">
-              <h2>Skills</h2>
-              <p className="skill-bx-p" style={{ color: "#FFFFFF" }}>
-                Take a look at some tools I've worked with.
-              </p>
-              <Carousel responsive={responsive} infinite={true}>
-                <div className="item">
-                  <ProjectCard projectId="witcher" />
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx">
+                            <h2>Skills</h2>
+                            <p className="skill-bx-p" style={{ color: "#FFFFFF" }}>
+                                Take a look at some tools I've worked with.
+                            </p>
+                            <Carousel responsive={responsive} infinite={true}>
+                                <div className="item">
+                                    <ProjectCard projectId="witcher" />
+                                </div>
+                                <div className="item">
+                                    <ProjectCard projectId="flight-scraper" />
+                                </div>
+                                <div className="item">
+                                    <ProjectCard projectId="witcher" />
+                                </div>
+                                <div className="item">
+                                    <ProjectCard projectId="flight-scraper" />
+                                </div>
+                            </Carousel>
+                        </div>
+                    </div>
                 </div>
-                <div className="item">
-                  <ProjectCard projectId="flight-scraper" />
-                </div>
-                <div className="item">
-                  <ProjectCard projectId="witcher" />
-                </div>
-                <div className="item">
-                  <ProjectCard projectId="flight-scraper" />
-                </div>
-              </Carousel>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default ProjectCarousel;
