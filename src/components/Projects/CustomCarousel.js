920ea805858cf9f2d09c6cd9595204f6b41c0c2e
Mateus <matbessa12@gmail.com>
2024-09-19 11:21:13 -0300

    Update CustomCarousel to Use Children and Separate Card Logic - Remove hardcoded data array from CustomCarousel component - Replace data mapping with React.Children.map for dynamic rendering - Introduce Card and CarouselWithCards components to manage and display project data

--- src/components/Projects/CustomCarousel.js
+++ src/components/Projects/CustomCarousel.js
@@ -5,42 +5,20 @@ import pandas from "../../assets/img/skills_icons/pandas.png";
 import java from "../../assets/img/skills_icons/java.png";
 import React, { useState } from 'react';
 
-const data = [
-    {
-        name: 'Placeholder Database',
-        img: database,
-        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-    },
-    {
-        name: 'Placeholder Flask',
-        img: flask,
-        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-    },
-    {
-        name: 'Placeholder Pandas',
-        img: pandas,
-        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-    },
-    {
-        name: 'Placeholder Java',
-        img: java,
-        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-    },
-];
-
-const CustomCarousel = () => {
-
+const CustomCarousel = ({ children }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [touchPosition, setTouchPosition] = useState(null)
+    const [touchPosition, setTouchPosition] = useState(null);
+
+    const totalSlides = React.Children.count(children);
 
     const prevSlide = () => {
         const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? data.length - 1 : currentIndex - 1;
+        const newIndex = isFirstSlide ? totalSlides - 1 : currentIndex - 1;
         setCurrentIndex(newIndex);
     };
 
     const nextSlide = () => {
-        const isLastSlide = currentIndex === data.length - 1;
+        const isLastSlide = currentIndex === totalSlides - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex);
     };
@@ -78,31 +56,16 @@ const CustomCarousel = () => {
             <div className="carousel-wrapper">
                 <div
                     className="carousel-inner"
-                    onTouchStart={handleTouchStart} // Add touch start handler
-                    onTouchMove={handleTouchMove}   // Add touch move handler
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
                 >
                     <div
                         className="carousel-track"
                         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
                     >
-                        {data.map((item, index) => (
+                        {React.Children.map(children, (child, index) => (
                             <div className="carousel-slide" key={index}>
-                                <div className="carousel-content">
-                                    <div className="carousel-image-container">
-                                        <img
-                                            src={item.img}
-                                            alt={item.name}
-                                            className="carousel-image"
-                                        />
-                                    </div>
-                                    <div className="carousel-text">
-                                        <p className="carousel-title">{item.name}</p>
-                                        <p className="carousel-review">{item.review}</p>
-                                        <button className="carousel-readmore-button">
-                                            Read more
-                                        </button>
-                                    </div>
-                                </div>
+                                {child}
                             </div>
                         ))}
                     </div>
@@ -120,4 +83,53 @@ const CustomCarousel = () => {
     );
 };
 
-export default CustomCarousel;
+// Card Component Logic
+const Card = ({ item }) => (
+    <div className="carousel-content">
+        <div className="carousel-image-container">
+            <img src={item.img} alt={item.name} className="carousel-image" />
+        </div>
+        <div className="carousel-text">
+            <p className="carousel-title">{item.name}</p>
+            <p className="carousel-review">{item.review}</p>
+            <button className="carousel-readmore-button">Read more</button>
+        </div>
+    </div>
+);
+
+// Sample data
+const data = [
+    {
+        name: 'Placeholder Database',
+        img: database,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Flask',
+        img: flask,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Pandas',
+        img: pandas,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Java',
+        img: java,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+];
+
+// Main Component that uses CustomCarousel and Card
+const CarouselWithCards = () => {
+    return (
+        <CustomCarousel>
+            {data.map((item, index) => (
+                <Card key={index} item={item} />
+            ))}
+        </CustomCarousel>
+    );
+};
+
+export default CarouselWithCards;
