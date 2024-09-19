43cc0c98ea3748fd3be311f2dbaf6e6b34aac726
Mateus <matbessa12@gmail.com>
2024-09-18 22:28:17 -0300

    removed tailwind from carousel

--- src/components/Projects/CustomCarousel.js
+++ src/components/Projects/CustomCarousel.js
@@ -1,4 +1,3 @@
-
 import './CustomCarousel.css';
 import database from "../../assets/img/skills_icons/database.png";
 import flask from "../../assets/img/skills_icons/old-flask.png";
@@ -6,29 +5,30 @@ import pandas from "../../assets/img/skills_icons/pandas.png";
 import java from "../../assets/img/skills_icons/java.png";
 import React, { useState } from 'react';
 
+const data = [
+    {
+        name: 'Placeholder Database',
+        img: database,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Flask',
+        img: flask,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Pandas',
+        img: pandas,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+    {
+        name: 'Placeholder Java',
+        img: java,
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
+    },
+];
+
 const CustomCarousel = () => {
-    const data = [
-        {
-            name: 'Placeholder Database',
-            img: database,
-            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-        },
-        {
-            name: 'Placeholder Flask',
-            img: flask,
-            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-        },
-        {
-            name: 'Placeholder Pandas',
-            img: pandas,
-            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-        },
-        {
-            name: 'Placeholder Java',
-            img: java,
-            review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, accusamus?',
-        },
-    ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -46,35 +46,35 @@ const CustomCarousel = () => {
 
     return (
         <div className="custom-carousel">
-            <div className="w-3/4 m-auto">
-                <div className="mt-20">
-                    <div className="bg-white h-[450px] text-black rounded-xl">
-                        <div className="rounded-t-xl bg-indigo-500 flex justify-center items-center">
+            <div className="carousel-wrapper">
+                <div className="carousel-inner">
+                    <div className="carousel-content">
+                        <div className="carousel-image-container">
                             <img
                                 src={data[currentIndex].img}
                                 alt={data[currentIndex].name}
-                                className="h-44 w-44 rounded-full border !border-yellow-500"
+                                className="carousel-image"
                             />
                         </div>
 
-                        <div className="flex flex-col justify-center items-center gap-4 p-4">
-                            <p className="text-xl font-semibold">{data[currentIndex].name}</p>
-                            <p>{data[currentIndex].review}</p>
-                            <button className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">
+                        <div className="carousel-text">
+                            <p className="carousel-title">{data[currentIndex].name}</p>
+                            <p className="carousel-review">{data[currentIndex].review}</p>
+                            <button className="carousel-readmore-button">
                                 Read more
                             </button>
                         </div>
                     </div>
-                    <div className="flex justify-between mt-4">
+                    <div className="carousel-navigation">
                         <button
                             onClick={prevSlide}
-                            className="bg-gray-500 text-white px-4 py-2 rounded"
+                            className="carousel-nav-button"
                         >
                             Previous
                         </button>
                         <button
                             onClick={nextSlide}
-                            className="bg-gray-500 text-white px-4 py-2 rounded"
+                            className="carousel-nav-button"
                         >
                             Next
                         </button>
@@ -85,4 +85,4 @@ const CustomCarousel = () => {
     );
 };
 
-export default CustomCarousel
+export default CustomCarousel;
