d04b3a758e495853e234fc66f5c63b8f2763bd9a
Mateus <matbessa12@gmail.com>
2024-09-24 14:00:36 -0300

    cleanup of contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -90,47 +90,47 @@ export const Contact = () => {
 
   return (
       <section className="contact" id="connect">
-        <Container>
-          <Row className="align-items-center">
-            <Col size={12} md={6} className="mx-auto">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-6 mx-auto">
               <TrackVisibility>
                 {({ isVisible }) =>
                     <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                       <h2>{textData.title}</h2>
                       <form onSubmit={handleSubmit}>
-                        <Row>
-                          {/*<Col size={12} sm={6} className="px-1">*/}
+                        <div className="row">
+                          {/*<div className="col-12 col-sm-6 px-1">*/}
                           {/*  <input type="text" value={formDetails.firstName} placeholder={textData.nameField} onChange={(e) => onFormUpdate('firstName', e.target.value)} />*/}
-                          {/*</Col>*/}
-                          {/*<Col size={12} sm={6} className="px-1">*/}
+                          {/*</div>*/}
+                          {/*<div className="col-12 col-sm-6 px-1">*/}
                           {/*  <input type="text" value={formDetails.lastName} placeholder={textData.surnameField} onChange={(e) => onFormUpdate('lastName', e.target.value)}/>*/}
-                          {/*</Col>*/}
-                          {/*<Col size={12} sm={6} className="px-1">*/}
+                          {/*</div>*/}
+                          {/*<div className="col-12 col-sm-6 px-1">*/}
                           {/*  <input type="email" value={formDetails.email} placeholder={textData.emailField} onChange={(e) => onFormUpdate('email', e.target.value)} />*/}
-                          {/*</Col>*/}
-                          {/*<Col size={12} sm={6} className="px-1">*/}
+                          {/*</div>*/}
+                          {/*<div className="col-12 col-sm-6 px-1">*/}
                           {/*  <input type="tel" value={formDetails.phone} placeholder={textData.phoneField} onChange={(e) => onFormUpdate('phone', e.target.value)}/>*/}
-                          {/*</Col>*/}
-                          <Col size={12} className="px-1">
+                          {/*</div>*/}
+                          <div className="col-12 px-1">
                             <textarea rows="6" value={formDetails.message} placeholder={textData.messageField} onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                             <button type="submit" className="whatsapp-button">
                               <FaWhatsapp style={{ marginRight: '8px', width: "1.5em", height: "1.5em"}} />
                               <span>{buttonText}</span>
                             </button>
-                          </Col>
+                          </div>
                           {
                               status.message &&
-                              <Col>
+                              <div className="col">
                                 <p className={status.success ? "success" : "danger"}>{status.message}</p>
-                              </Col>
+                              </div>
                           }
-                        </Row>
+                        </div>
                       </form>
                     </div>}
               </TrackVisibility>
-            </Col>
-          </Row>
-        </Container>
+            </div>
+          </div>
+        </div>
       </section>
   )
 }
