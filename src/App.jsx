d6ff87f9ae1216b55c49ddb116afe9469e5baea8
Mateus <matbessa12@gmail.com>
2025-06-17 11:42:27 -0300

    Refactor App component; remove unused styles and integrate Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LanguageContext from './components/LanguageContext';
-import { Routes, Route } from "react-router-dom";
-import FullLayout from "./components/FullLayout.jsx";
 
 function App() {
-    const [language, setLanguage] = useState('english');
-    const [selectedFlag, setSelectedFlag] = useState('usa');
+  const [count, setCount] = useState(0)
 
-    return (
-        <LanguageContext.Provider value={{ selectedFlag, setSelectedFlag, language, setLanguage }}>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<FullLayout />} />
-                    <Route path="*" element={
-                        <div style={{
-                            color: 'white',
-                            fontSize: '2rem',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            height: '100vh',
-                            backgroundColor: '#1a1a1a'
-                        }}>
-                            404 - Route not matched
-                        </div>
-                    } />
-                </Routes>
-            </div>
-        </LanguageContext.Provider>
-    );
+  return (
+    <div className="text-3xl">
+        Hello world!
+    </div>
+  )
 }
 
-export default App;
+export default App
