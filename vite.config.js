6ba44fed2382be543e61278953db819398d60507
Mateus <matbessa12@gmail.com>
2025-06-23 22:24:03 -0300

    bbbbbbbbbbb

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Detect branch (default to 'main')
-const branch = process.env.BRANCH || 'main'
-
-// Adjust base path depending on branch
-const base =
-    branch === 'carousel-refactor'
-        ? '/mateus_portfolio/' // GitHub Pages always uses the same base
-        : '/mateus_portfolio/' // You can make this conditional if you had previews like '/mateus_portfolio/preview/'
-
+// https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: '/mateus_portfolio/',
   plugins: [react(), tailwindcss()],
 })
